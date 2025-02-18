069e8a5b1f3be756798b085172fd364f96d63f3d
Michel Nass <michel.nass@bth.se>
2025-02-18 13:33:42 +0100

    Small change 2

--- src/test/java/com/example/HelloWorldTest.java
+++ src/test/java/com/example/HelloWorldTest.java
@@ -2,11 +2,12 @@ package test.java.com.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import main.java.com.example.HelloWorld;
 
 public class HelloWorldTest {
     
     @Test
     void testGetMessage() {
-        assertEquals("Hello from Jenkins!", HelloWorld.getMessage());
+        assertEquals("Hello from Jenkins 2!", HelloWorld.getMessage());
     }
 }
